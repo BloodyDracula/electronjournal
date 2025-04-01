fff847ab21269077e6172a3dc6f6ad904434cd61
BloodyDracula <feskabanec@gmail.com>
2025-04-01 19:35:12 +0200

    edit

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    presets: [
-        '@vue/cli-plugin-babel/preset', // Используем пресет Vue CLI для Babel
-    ],
-};
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // Импортируем router
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap'; // Если нужны JavaScript-компоненты Bootstrap
-
-createApp(App)
-    .use(router) // Подключаем Vue Router
-    .mount('#app');
